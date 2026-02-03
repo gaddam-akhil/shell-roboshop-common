8e92a98d48f209a0b17ff29cde11810763a405bd
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-03 21:58:20 +0530

    common

---
+++ redis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+APP_NAME=redis
+source ./common.sh
+
+check_root
+
+dnf module disable redis -y &>>$LOGS_FILES
+dnf module enable redis:7 -y &>>$LOGS_FILES
+VALIDATE $? "enable redis" 
+
+dnf install redis -y &>>$LOGS_FILES
+VALIDATE $? "installing redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e  '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "ALLOWING REMOTE CONNNECTIN"
+
+systemctl enable redis 
+systemctl start redis &>>$LOGS_FILES
+VALIDATE $? "start redis"
+
+print_total_time
