813e5c739718c5a3a2a0246cde1adad797fc76fd
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-04 22:49:59 +0530

    common

--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+source ./common.sh
+
 
 APP_NAME=mysql
-source ./common.sh
 check_root
 
 
 
-dnf install mysql-server -y 
+dnf install mysql-server -y &>>$LOGS_FILES
 VALIDATE $? "Installing mysql"
 
 systemctl enable mysqld &>>$LOGS_FILES
@@ -14,8 +15,8 @@ VALIDATE $? "ENABLE MYSQL"
 
 systemctl start mysqld  &>>$LOGS_FILES
 VALIDATE $? "START MYSQL"
-
+#get the password from user
 mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOGS_FILES
-VALIDATE $? "CHANGING PASWORD"
+VALIDATE $? "setup root PASWORD"
 
 print_total_time
