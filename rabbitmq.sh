ee7c41750eb79768fe4b969b23e8e52357b548fc
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-04 19:02:36 +0530

    common

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -5,16 +5,16 @@ source ./common.sh
 
 check_root
 
-cp $SCRIPT_DIR/$APP_NAME.repo  /etc/yum.repos.d/$APP_NAME.repo
+cp $SCRIPT_DIR/rabbitmq.repo  /etc/yum.repos.d/rabbitmq.repo
 VALIDATE $? "ADDING REPO FILE"
 
-dnf install $APP_NAME-server -y &>>$LOGS_FILES
-VALIDATE $? "Installing $APP_NAME sever"
+dnf install rabbitmq-server -y &>>$LOGS_FILES
+VALIDATE $? "Installing rabbitmq sever"
 
-systemctl enable $APP_NAME-server &>>$LOGS_FILES
-systemctl start $APP_NAME-server &>>$LOGS_FILES
+systemctl enable rabbitmq-server &>>$LOGS_FILES
+systemctl start rabbitmq-server &>>$LOGS_FILES
 VALIDATE $? "ENABLING AND STARTING SERVER"
 
-$APP_NAMEctl add_user roboshop roboshop123 &>>$LOGS_FILES
-$APP_NAMEctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGS_FILES
+rabbitmqctl add_user roboshop roboshop123 &>>$LOGS_FILES
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGS_FILES
 VALIDATE $? "ADDING USER AND SETTING PERMISSION"
