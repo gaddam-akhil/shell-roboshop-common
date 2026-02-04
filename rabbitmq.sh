d4d82de8bc598d3ffd8d4e72c4224f17d85ffd4f
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-04 18:58:00 +0530

    common

---
+++ rabbitmq.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+APP_NAME=rabbitmq
+source ./common.sh
+
+check_root
+
+cp $APP_NAME.repo  /etc/yum.repos.d/$APP_NAME.repo
+VALIDATE $? "ADDING REPO FILE"
+
+dnf install $APP_NAME-server -y &>>$LOGS_FILES
+VALIDATE $? "Installing $APP_NAME sever"
+
+systemctl enable $APP_NAME-server &>>$LOGS_FILES
+systemctl start $APP_NAME-server &>>$LOGS_FILES
+VALIDATE $? "ENABLING AND STARTING SERVER"
+
+$APP_NAMEctl add_user roboshop roboshop123 &>>$LOGS_FILES
+$APP_NAMEctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOGS_FILES
+VALIDATE $? "ADDING USER AND SETTING PERMISSION"
