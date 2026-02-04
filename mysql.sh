959acda2fea345c2976cae632230cd46a18a3ca7
gaddam akhil <gaddamakhil900@gmail.com>
2026-02-04 18:51:04 +0530

    common

--- mysql.sh
+++ mysql.sh
@@ -11,8 +11,10 @@ dnf install mysql-server -y &>>$LOGS_FILES
 VALIDATE $? "Installing mysql"
 
 systemctl enable mysqld &>>$LOGS_FILES
+VALIDATE $? "ENABLE MYSQL"
+
 systemctl start mysqld  &>>$LOGS_FILES
-VALIDATE $? "ENABLE AND START MYSQL"
+VALIDATE $? "START MYSQL"
 
 mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOGS_FILES
 VALIDATE $? "CHANGING PASWORD"
